d7f7d25d1a01f5f31836bd4431590e6d4803fb9b
student <student@ip-172-31-56-36.us-west-2.compute.internal>
2022-05-03 18:28:19 -0700

    6:28 PM 5/3/22

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -1,14 +1,21 @@
 package _01_methods._5_FlappyBird;
 
+import java.util.Random;
+
 import processing.core.PApplet;
 
 public class FlappyBird extends PApplet {
 	static final int WIDTH = 800;
 	static final int HEIGHT = 600;
+	Random random = new Random();
+	int ran = random.nextInt(300 - 50) - 50;
 	int birdX = 350;
 	int birdY = 0;
-	int birdYVelocity = -10;
+	int birdYVelocity = -3;
 	int gravity = 1;
+	int pipeX = 600;
+	int pipeGap = 100;
+	int lowerPipeY = ran + pipeGap;
 
 	@Override
 	public void settings() {
@@ -17,24 +24,39 @@ public class FlappyBird extends PApplet {
 
 	@Override
 	public void setup() {
-		
+		teleportPipes();
 	}
 
 	@Override
 	public void draw() {
 		birdY += birdYVelocity;
 		birdYVelocity += gravity;
-		fill(42,212,87);
-		rect
+		pipeX -= 2;
 		background(135, 206, 250);
+		fill(42, 212, 87);
+		rect(pipeX, 0, 50, ran);
+		rect(pipeX, 600, 50, lowerPipeY * -1);
 		fill(155, 135, 12);
 		stroke(0, 0, 0);
 		ellipse(birdX, birdY, 100, 80);
+		teleportPipes();
 	}
 
 	public void mousePressed() {
 		if (mousePressed) {
-			birdYVelocity = -10;
+			birdYVelocity = -7;
+		}
+	}
+
+	public void teleportPipes() {
+		if (pipeX < 100) {
+			pipeX = 700;
+			ran = (int) random(25) * 10;
+		}
+	}
+	boolean pipeHitCheck() {
+		if(birdY < ran && birdX > pipeX && birdX < (pipeX+50)) {
+			
 		}
 	}
 
