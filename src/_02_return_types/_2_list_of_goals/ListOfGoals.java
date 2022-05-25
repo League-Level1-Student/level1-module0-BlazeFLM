3fbcd7b05fcb9503840b231f357f3e3c92242971
student <student@ip-172-31-49-248.us-west-2.compute.internal>
2022-05-24 18:25:12 -0700

    6:25 PM 5/24/22

--- src/_02_return_types/_2_list_of_goals/ListOfGoals.java
+++ src/_02_return_types/_2_list_of_goals/ListOfGoals.java
@@ -1,52 +1,54 @@
 package _02_return_types._2_list_of_goals;
 
-
-
 import javax.swing.JOptionPane;
 
 public class ListOfGoals {
-	static String listOfGoals ="";
+	static String listOfGoals = "";
 	static int numberOfGoals = 0;
-	
+
 	public static void main(String[] args) {
-		//1. Call the appropriate method to make your list of goals
-		
-		//2. Call the appropriate method to get your list of goals, 
-		//   so you can use a pop-up to display them
-		
-		//3. Call the appropriate method to get the number of goals you entered,
-		//   and using a pop-up display the number of goals
-		
-		//4. Call the appropriate method to see if you put a lot of goals,
-		//   if you did put a lot of goals use a pop-up to say "Wow that's a lot of goals! Awesome!"
-		
+		// 1. Call the appropriate method to make your list of goals
+		createListOfGoals();
+		// 2. Call the appropriate method to get your list of goals,
+		// so you can use a pop-up to display them
+		getListOfGoals();
+		// 3. Call the appropriate method to get the number of goals you entered,
+		// and using a pop-up display the number of goals
+		getNumberOfGoals();
+		// 4. Call the appropriate method to see if you put a lot of goals,
+		// if you did put a lot of goals use a pop-up to say "Wow that's a lot of goals!
+		// Awesome!"
+		isManyGoals();
+		if (isManyGoals() == true) {
+			JOptionPane.showMessageDialog(null, "Wow that's a lot of goals! Awesome!");
+		}
 	}
-	
+
 	static void createListOfGoals() {
 		String input = "";
-		while(true) {
+		while (true) {
 			input = JOptionPane.showInputDialog("Enter a goal you have\n(Or X to stop entering goals)");
-			if( input.equalsIgnoreCase("X") ) {
+			if (input.equalsIgnoreCase("X")) {
 				break;
 			}
-			listOfGoals += input+ "\n";
+			listOfGoals += input + "\n";
 			numberOfGoals++;
 		}
 	}
-	
+
 	static String getListOfGoals() {
 		return listOfGoals;
 	}
-	
+
 	static int getNumberOfGoals() {
 		return numberOfGoals;
 	}
-	
+
 	static boolean isManyGoals() {
-		if( getNumberOfGoals() > 5) {
+		if (getNumberOfGoals() > 5) {
 			return true;
 		}
 		return false;
 	}
-	
+
 }
--- src/_02_return_types/_3_too_many_shapes/TooManyShapes.java
+++ src/_02_return_types/_3_too_many_shapes/TooManyShapes.java
@@ -1,7 +1,5 @@
 package _02_return_types._3_too_many_shapes;
 
-
-
 import java.util.Random;
 
 import javax.swing.JOptionPane;
@@ -12,36 +10,42 @@ public class TooManyShapes {
 	static Robot rob = new Robot();
 
 	public static void main(String[] args) {
-		//1. Ask the user how many sides they want their shape to be
-		
-		//2. Ask the user how many shapes they want
-		
-		//3. Call canMakeShape() and save what is returned into a variable
-		
-		//4. If the shape CAN be drawn
-		
-			//5. Call and save what is returned from calculateTurnAngle()
-		
-			//6. Use drawPolygons() to draw your shape
-		
-		//7. If the shape CANNOT be drawn 
-			
-			//8. Call notEnoughSides() and print out what is returned 
-		
+		// 1. Ask the user how many sides they want their shape to be
+		String sides = JOptionPane.showInputDialog("How many sides on your shape?");
+		int sideInt = Integer.parseInt(sides);
+		String deger = JOptionPane.showInputDialog("How many degrees do you want your shape to have?");
+		int degrees = Integer.parseInt(deger);
+		// 2. Ask the user how many shapes theywant
+		String shapes = JOptionPane.showInputDialog("How many shapes do you want?");
+		int shapeInt = Integer.parseInt(shapes);
+		// 3. Call canMakeShape() and save what is returned into a variable
+		canMakeShape(shapeInt);
+		// 4. If the shape CAN be drawn
+		if (canMakeShape(shapeInt)) {
+			// 5. Call and save what is returned from calculateTurnAngle()
+			calculateTurnAngle(sideInt);
+		}
+		// 6. Use drawPolygons() to draw your shape
+		drawPolygons(sideInt, shapeInt, degrees);
+		// 7. If the shape CANNOT be drawn
+		if (!canMakeShape(shapeInt)) {
+			// 8. Call notEnoughSides() and print out what is returned
+			System.out.println(notEnoughSides());
+		}
 	}
-	
+
 	static int calculateTurnAngle(int numSides) {
-		int angle = 360/numSides;
+		int angle = 360 / numSides;
 		return angle;
 	}
-	
+
 	static void drawPolygons(int numSides, int numShapes, int degrees) {
 		rob.setSpeed(100);
 		rob.penDown();
 		int dist = 25;
 		Random rand = new Random();
-		
-		for(int i = 0; i < numShapes; i++) {
+
+		for (int i = 0; i < numShapes; i++) {
 			int x = rand.nextInt(600);
 			int y = rand.nextInt(600);
 			int angle = rand.nextInt(360);
@@ -50,24 +54,23 @@ public class TooManyShapes {
 			rob.setY(y);
 			rob.setAngle(angle);
 			rob.setRandomPenColor();
-			
-			for(int j = 0; j < numSides; j++) {
+
+			for (int j = 0; j < numSides; j++) {
 				rob.move(dist);
 				rob.turn(degrees);
 			}
-	    }
+		}
 		rob.hide();
 	}
-	
+
 	static boolean canMakeShape(int numSides) {
-		if(numSides >= 3) {
+		if (numSides >= 3) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	static String notEnoughSides() {
-		return "I'm sorry we cannot make a shape with that many sides\n"
-				+ "Please enter a number greater than 2";
+		return "I'm sorry we cannot make a shape with that many sides\n" + "Please enter a number greater than 2";
 	}
 }
