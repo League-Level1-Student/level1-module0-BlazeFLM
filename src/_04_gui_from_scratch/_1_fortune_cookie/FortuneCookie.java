7e685965d1a1e23120a7c5f550bd9d58ad27ae46
student <student@ip-172-31-53-237.us-west-2.compute.internal>
2022-06-28 17:40:31 -0700

    NOT FULL COMMIT, WILL RETURN 6:30

--- src/_04_gui_from_scratch/_1_fortune_cookie/FortuneCookie.java
+++ src/_04_gui_from_scratch/_1_fortune_cookie/FortuneCookie.java
@@ -1,9 +1,19 @@
 package _04_gui_from_scratch._1_fortune_cookie;
 
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class FortuneCookie {
 	public void showButton() {
 		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		JButton button = new JButton();
+		frame.add(button);
+		frame.pack();
+	
+		button.addActionListener((ActionListener) this);
+		
 	}
 }
