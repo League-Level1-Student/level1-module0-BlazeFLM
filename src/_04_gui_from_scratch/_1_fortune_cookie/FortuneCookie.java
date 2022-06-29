4a8bc36ea409c9d80f78789b45eb25cf5cbdb320
student <student@ip-172-31-63-24.us-west-2.compute.internal>
2022-06-28 20:00:37 -0700

    8:00 PM 6/28/22

--- src/_04_gui_from_scratch/_1_fortune_cookie/FortuneCookie.java
+++ src/_04_gui_from_scratch/_1_fortune_cookie/FortuneCookie.java
@@ -1,19 +1,40 @@
 package _04_gui_from_scratch._1_fortune_cookie;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class FortuneCookie {
+public class FortuneCookie implements ActionListener {
 	public void showButton() {
 		JFrame frame = new JFrame();
 		frame.setVisible(true);
 		JButton button = new JButton();
 		frame.add(button);
 		frame.pack();
-	
-		button.addActionListener((ActionListener) this);
 		
+		button.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("button pressed");
+		JOptionPane.showMessageDialog(null, "Woohoo");
+		int ran = new Random().nextInt(5);
+		if(ran == 0) {
+			JOptionPane.showMessageDialog(null, "The fortune you seek is in another cookie.");
+		} else if (ran == 1) {
+			JOptionPane.showMessageDialog(null, "A foolish man listens to his heart. A wise man listens to cookies.");
+		} else if (ran == 2) {
+			JOptionPane.showMessageDialog(null, "If you look back, you’ll soon be going that way.");
+		} else if (ran == 3) {
+			JOptionPane.showMessageDialog(null, "You will be hungry again in one hour.");
+		} else {
+			JOptionPane.showMessageDialog(null, "You can always find happiness at work on Friday.");
+		}
 	}
 }
